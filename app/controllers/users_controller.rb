d7a0a80f04b658fea4775f92c70aea6ab57ab742
github username <github email address>
2019-04-16 12:16:01 +0100

    set default categories for user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,33 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-
     if @user
       render json: @user
     else
       render json: {error: "User not found"}, status: 404
     end
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      defaults= [
+        {name: "Health", color: "pink", user: @user},
+        {name: "Business", color: "blue", user: @user},
+        {name: "Social", color: "yellow", user: @user},
+        {name: "Appointments", color: "green", user: @user}
+      ]
+      defaults.each {|obj| Category.create(obj)}
+      render json: @user
+    else
+      render json: {error: "Failed to create User" }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :categories
   has_many :notes, through: :categories
+
+  validates :username, uniqueness: true
+  validates :username, presence: true
 end
